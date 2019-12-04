565e3192618f7ab43a0be971a2e9e9d5cfc37799
Anisa <anisahamohamed@gmail.com>
2019-12-04 22:26:11 +0000

    added input.js

---
+++ input.js
@@ -0,0 +1,23 @@
+
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key)
+  });
+  return stdin;
+}
+
+
+
+const handleUserInput= (data)=>{
+  if (data === '\u0003') { // \u0003 maps to ctrl+c input
+    console.log("Thanks for using me, ciao!")
+
+    process.exit();}
+
+}
+module.exports = {setupInput}
